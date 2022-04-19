17b7a2051421e24ae5ab9c5b4059a05d81834352
taiseidayo <tkapple0827aaa@gmail.com>
2022-04-19 20:59:17 +0900

    once commit.

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.Server(app);
+const io = socketIo(server);
+
+const PORT = 3100;
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+  socket.on("sendMessage", (message) => {
+    console.log("Message has been sent: ", message);
+
+    // 'receiveMessage' というイベントを発火、受信したメッセージを全てのクライアントに対して送信する
+    io.emit("receiveMessage", message);
+  });
+});
